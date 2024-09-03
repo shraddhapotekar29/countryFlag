9b28c4b941c710b9b0786ad0a0619000fbe14490
shraddha potekar <shraddhapotekar762@gmail.com>
2024-09-03 22:12:20 +0530

    first commit

---
+++ xcountries/src/App.js
@@ -0,0 +1,12 @@
+import Xcountries from './xcountries';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Xcountries/>
+    </div>
+  );
+}
+
+export default App;
---
+++ xcountries/src/xcountries.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import style from "./xcountries.module.css"
+const Xcountries=()=>{
+    const[flagData,setFlagData]=useState([]);
+    const getFlagData=async()=>{
+        try{
+     const response=await axios.get("https://xcountries-backend.azurewebsites.net/all");
+     setFlagData(response.data);
+     console.log(response.data);
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+useEffect(()=>{
+    getFlagData();
+},[]);
+    return(<div className={style.mainDiv}>
+          {flagData.map((flag)=>(<div className={style.box}>
+            <img src={flag.flag} alt={flag.abbr}  height={80} width={80} style={{paddingTop:"1rem"}}/>
+            <p>{flag.name}</p>
+        </div>))}
+    </div>)
+}
+export default Xcountries;
